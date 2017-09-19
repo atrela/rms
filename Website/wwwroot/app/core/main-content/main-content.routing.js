ce3b1a3d26cc8ff5984f690e031fa312645a310f
Marek Zajac <marek.a.zajac@capgemini.com>
2017-09-19 22:42:10 +0200

    Remove asp net core website

--- Website/wwwroot/app/core/main-content/main-content.routing.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var router_1 = require("@angular/router");
-var chart_component_1 = require("./chart/chart.component");
-var routes = [
-    { path: 'chart/:name', component: chart_component_1.ChartComponent, runGuardsAndResolvers: 'always' }
-];
-exports.routing = router_1.RouterModule.forRoot(routes);
-//# sourceMappingURL=main-content.routing.js.map
